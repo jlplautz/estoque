447388f19590dcd2155a40d6cd251faa0715ea9d
61246880 <jorge.plautz@gmail.com>
2020-05-12 01:06:37 -0300

    Commit to heroku

--- projeto/settings.py
+++ projeto/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv()) <- old
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = ['*']
+
 
 # Application definition
 
