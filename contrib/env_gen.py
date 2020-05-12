2f1a0c8c46f382042e921345e74627e37f140492
61246880 <jorge.plautz@gmail.com>
2020-05-12 01:17:47 -0300

    Commit to heroku

--- contrib/env_gen.py
+++ contrib/env_gen.py
@@ -1,18 +1,7 @@
-#!/usr/bin/env python
-
-"""
-Django SECRET_KEY generator.
-"""
-from django.utils.crypto import get_random_string
-
-
-chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-
-CONFIG_STRING = """
-DEBUG=True
-SECRET_KEY=%s
-ALLOWED_HOSTS=127.0.0.1, .localhost
-#DATABASE_URL=postgres://USER:PASSWORD@HOST:PORT/NAME
+DEBUG=False
+SECRET_KEY=defina sua chave secreta
+ALLOWED_HOSTS=localhost, 127.0.0.1
+DATABASE_URL=postgres://postgres:postgres@localhost/stockdb
 #DEFAULT_FROM_EMAIL=
 #EMAIL_BACKEND=django.core.mail.backends.smtp.EmailBackend
 #EMAIL_HOST=
@@ -20,8 +9,3 @@ ALLOWED_HOSTS=127.0.0.1, .localhost
 #EMAIL_USE_TLS=
 #EMAIL_HOST_USER=
 #EMAIL_HOST_PASSWORD=
-""".strip() % get_random_string(50, chars)
-
-# Writing our configuration file to '.env'
-with open('.env', 'w') as configfile:
-    configfile.write(CONFIG_STRING)
