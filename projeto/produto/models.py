d94e0d75f775291523d38d127d40afc22c91b39d
61246880 <jorge.plautz@gmail.com>
2020-05-18 18:16:29 -0300

    Commit to heroku

--- projeto/produto/models.py
+++ projeto/produto/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse_lazy
 
 class Produto(models.Model):
     importado = models.BooleanField(default=False)
-    ncm = models.CharField('NCM', max_length=8)
+    ncm = models.CharField('NCM', max_length=13)
     produto = models.CharField(max_length=100, unique=True)
     preco = models.DecimalField('preço', max_digits=7, decimal_places=2)
     estoque = models.IntegerField('estoque atual')
